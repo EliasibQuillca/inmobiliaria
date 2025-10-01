00452b863c49901758c7d25226365798d98e8fde
Eliasib Noe <eliasdragon159@gmail.com>
2025-09-30 23:16:00 -0500

    limpiesa necesaria de archivos rendundates

--- fix_imports.ps1
+++
@@ -1,6 +0,0 @@
-$files = Get-ChildItem -Path 'resources/js/Pages' -Recurse -Filter "*.jsx"
-foreach ($file in $files) {
-    $content = Get-Content $file.FullName
-    $newContent = $content -replace "import Layout from '@/Components/Layout/Layout';", "import Layout from '@/components/layout/Layout';"
-    Set-Content -Path $file.FullName -Value $newContent
-}
